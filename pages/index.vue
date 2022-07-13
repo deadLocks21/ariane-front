1567dad787747605986bd7a72ef14f8228e01200
deadLocks21 <timothe@hofmann.fr>
2022-07-13 22:03:27 +0200

    Tailwind CSS and DaisyUI

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
-  <Tutorial/>
+  <div>
+    <button class="btn">Button</button>
+    <button class="btn btn-primary">Button</button>
+    <button class="btn btn-secondary">Button</button>
+    <button class="btn btn-accent">Button</button>
+    <button class="btn btn-ghost">Button</button>
+    <button class="btn btn-link">Button</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
